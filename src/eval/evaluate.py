b2bd564c09f8ea8e972fccb521f446c8a34df7e8
Ngo Quang Viet <viet.nq194881@sis.hust.edu.vn>
2024-04-04 09:37:20 +0700

    code evaluate.py

--- src/eval/evaluate.py
+++ src/eval/evaluate.py
@@ -1,3 +1,37 @@
+from model import NERModel
+from tokenizer import NERTokenizer
+from data import NERDataset
+from serve import inference
+
+import argparse
+
+
 class NEREvaluator:
     def evaluate(self, preds, labels):
         return "evaluation result"
+
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_id")
+    parser.add_argument("--max_length", default="512")
+
+    args = parser.parse_args()
+    model_id = args.model_id
+    max_length = int(args.max_length)
+
+    # Load model
+    NER_model = NERModel("evaluation")
+    NER_model.from_pretrained(model_id)
+
+    NER_tokenizer = NERTokenizer()
+    NER_tokenizer.from_pretrained(model_id)
+
+    eval_ds = NERDataset()
+    eval_ds.load_dataset(
+        path="json", data_files="test_data/CrossNER_AI.json", split="train"
+    )
+    eval_ds.convert_dataset("conversations", "instruction")
+
+    print(eval_ds.dataset[0])
--- src/model.py
+++ src/model.py
@@ -2,22 +2,24 @@ from transformers import (
     AutoModel,
     T5ForConditionalGeneration,
 )
+import torch
 
 
 class NERModel:
-
-    def __init__(self, usage='train'):
-        if usage == 'inference':
+    def __init__(self, usage="train"):
+        if usage == "inference" or usage == "evaluation":
             self.setup = self.eval
         else:
-            self.setup = lambda x: None
+            self.setup = lambda: None
 
     def from_pretrained(self, model_id):
         if "t5" in model_id:
             model_class = T5ForConditionalGeneration
         else:
             model_class = AutoModel
-        self.model = model_class.from_pretrained(model_id)
+        model = model_class.from_pretrained(model_id)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = model.to(device)
         self.model.cuda()
         self.setup()
 
